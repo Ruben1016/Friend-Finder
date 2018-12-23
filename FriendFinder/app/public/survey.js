67c96783a069c0d33b446b8529eda7518bc89b6d
Ruben <rnegrete1016@gmail.com>
2018-12-23 11:12:41 -0600

    updated

---
+++ FriendFinder/app/public/survey.js
@@ -0,0 +1,5 @@
+// one someone submits the form need to need to validate they submit inpuy
+// need to create a new user, post that to the server
+// Server is going to reply with the best match
+// display the best match
+// 
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -2,15 +2,15 @@ var mysql = require("mysql")
 
 var connection;
 if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "finder_db"
-});
+     var connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "finder_db"
+    });
 }
 
 connection.connect(function(err) {
@@ -19,3 +19,42 @@ if (err) {
   //once successfully connected, you may want to query your database for the info you'll need later!
 }
 });
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        connection.query ("SELECT * FROM profiles", function (err, response) {
+            if (err) {
+            console.log(err);
+            
+            }
+            res.json(response)
+        })
+    
+    })
+
+    app.post("/api/friends", function(req, res){
+        var friendsList;
+        connection.query ("SELECT * FROM profiles", function (err, response) {
+            if (err) {
+            console.log(err);
+            
+            }
+           friendsList=response
+        })
+        var newFriend= req.body
+        var bestMatch;
+        var bestScore=100
+
+    //    loop through the freinds list 
+    // compare each score to current friends score
+    // take total difference and compare to current best score
+    // if less then current best score then this profile becomes best match
+        // add new profile to the db, responds with the best match 
+        // create a new friend in postman to test it
+        // add to code to add new friends to db and work logic
+
+        res.json(friendsList)
+    })
+
+}
+
